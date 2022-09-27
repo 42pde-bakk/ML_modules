bd674860d146b9fbe8d965c386a5c7ce1f6e01a5
pde-bakk <pde-bakk@student.codam.nl>
2022-09-27 17:22:34 +0200

    fixed tests, now all i still need is Vectors I think

--- module00/ex00/test.py
+++ module00/ex00/test.py
@@ -2,21 +2,24 @@ from matrix import Matrix
 
 
 def main() -> None:
-    matrix = Matrix((3, 3))
-    # print(f'{matrix.shape = }')
-    # print(f'{matrix.data = }')
-    assert matrix.shape == (3, 3)
-
+    # Simple constructor and Matrix.T() tests
+    assert Matrix((3, 3)).shape == (3, 3), 'Error in Matrix((3, 3))'
     m1 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
-    print(f'{m1.shape = }')
-    print(f'{m1.data = }')
-    assert m1.shape == (3, 2)
+    assert m1.shape == (3, 2), 'Shape wrong'
+    assert m1.data == [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]]
 
     m1_t = m1.T()
     assert m1_t.shape == (2, 3)
+    assert m1_t.data == [[0.0, 2.0, 4.0], [1.0, 3.0, 5.0]]
 
     m1 = Matrix([[0., 2., 4.], [1., 3., 5.]])
     assert m1.shape == (2, 3)
+
+    m1_t = m1.T()
+    assert m1_t.shape == (3, 2)
+    assert m1_t.data == [[0., 1.], [2., 3.], [4., 5.]]
+
+    # Let's test matrix multiplication
 
     m1 = Matrix([[0.0, 1.0, 2.0, 3.0],
                  [0.0, 2.0, 4.0, 6.0]])
@@ -27,6 +30,8 @@ def main() -> None:
 
     matmult = m1 * m2
     print(matmult)
+    assert matmult.shape == (2, 2)
+    assert matmult.data == [[28., 34.], [56., 68]]
 
 
 if __name__ == '__main__':
