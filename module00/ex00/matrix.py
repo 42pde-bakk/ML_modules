4c50b171c4c8a2f514e533b82ddb7575d1e66d61
pde-bakk <pde-bakk@student.codam.nl>
2022-09-27 19:57:13 +0200

    matrix and vector done I think

--- module00/ex00/matrix.py
+++ module00/ex00/matrix.py
@@ -2,10 +2,14 @@ from __future__ import annotations
 from copy import deepcopy
 
 
+def get_matrix_or_vector(data: list[list]) -> Matrix | Vector:
+	if len(data) == 1 or len(data[0]) == 1:
+		return Vector(data)
+	return Matrix(data)
+
+
 class Matrix:
-	def __init__(self, arg):
-		self.data = []  # List of lists
-		self.shape = tuple
+	def __init__(self, arg: tuple | list):
 		if isinstance(arg, tuple):
 			if len(arg) != 2:
 				raise RuntimeError('Bad tuple given to Matrix constructor')
@@ -18,11 +22,16 @@ class Matrix:
 		else:
 			raise NotImplementedError
 
+	def _is_vector(self):
+		if any(x == 0 for x in self.shape):
+			return False
+		return self.shape[0] == 1 or self.shape[1] == 1
+
 	def __add__(self, other: Matrix):
 		if not isinstance(other, Matrix) or self.shape != other.shape:
 			raise TypeError("add: only matrices of same dimensions")
-		data = [[self.data[col][idx] + other.data[col][idx] for col in range(self.shape[0])] for idx in range(self.shape[1])]
-		return Matrix(data)
+		data = [[col + col2 for col, col2 in zip(row, row2)] for row, row2 in zip(self.data, other.data)]
+		return get_matrix_or_vector(data)
 
 	def __radd__(self, other):
 		return self.__add__(other)
@@ -33,25 +42,25 @@ class Matrix:
 	def __rsub__(self, other):
 		return self.__rsub__(other)
 
-	def __truediv__(self, other: int | float) -> Matrix:
+	def __truediv__(self, other: int | float) -> Matrix | Vector:
 		if not isinstance(other, (int, float)):
 			raise TypeError("div: only scalars")
 		data = [[self.data[col][idx] / other for col in range(self.shape[0])] for idx in range(self.shape[1])]
-		return Matrix(data)
+		return get_matrix_or_vector(data)
 
 	def __rtruediv__(self, other):
 		return NotImplemented
 
-	def __mul__(self, other: Matrix | int | float) -> Matrix:
+	def __mul__(self, other: Matrix | int | float) -> Matrix | Vector:
 		if isinstance(other, Matrix):
 			if self.shape[1] != other.shape[0]:
 				raise ValueError('The number of columns in matrix a has to be equal to the number of rows in matrix b')
 			bt = other.T()
 			data = [[sum(a * b for a, b in zip(row, row2)) for row2 in bt.data] for row in self.data]
-			return Matrix(data)
+			return get_matrix_or_vector(data)
 		elif isinstance(other, (int, float)):
 			data = [[self.data[col][idx] * other for col in range(self.shape[0])] for idx in range(self.shape[1])]
-			return Matrix(data)
+			return get_matrix_or_vector(data)
 		return NotImplemented
 
 	def __rmul__(self, other):
@@ -63,6 +72,28 @@ class Matrix:
 	def __repr__(self):
 		return self.__str__()
 
-	def T(self) -> Matrix:
+	def T(self) -> Matrix | Vector:
 		data = [[self.data[col][idx] for col in range(self.shape[0])] for idx in range(self.shape[1])]
-		return Matrix(data)
+		return get_matrix_or_vector(data)
+
+
+class Vector(Matrix):
+	def __init__(self, arg: list | Matrix | Vector):
+		if isinstance(arg, Matrix):
+			super().__init__(arg.data)
+		else:
+			super().__init__(arg)
+		if self.shape[0] != 1 and self.shape[1] != 1:
+			raise TypeError('ERROR: You must pass a row or column vector as arg')
+
+	def dot(self, v: Vector) -> int | float | None:
+		if self.shape not in (v.shape, v.shape[::-1]):
+			print('Please make sure the shapes of both vectors match!')
+			return None
+		if self.shape == v.shape[::-1]:
+			v = v.T()
+		tot = 0
+		for row, row2 in zip(self.data, v.data):
+			for col, col2 in zip(row, row2):
+				tot += col * col2
+		return sum(sum(a * b for a, b in zip(row, row2)) for row, row2 in zip(self.data, v.data))
--- module00/ex00/test.py
+++ module00/ex00/test.py
@@ -1,4 +1,4 @@
-from matrix import Matrix
+from matrix import Matrix, Vector
 
 
 def main() -> None:
@@ -12,6 +12,8 @@ def main() -> None:
 	assert m1_t.shape == (2, 3)
 	assert m1_t.data == [[0.0, 2.0, 4.0], [1.0, 3.0, 5.0]]
 
+	# Maybe also do some tests on __repr__ and __str__ ?
+
 	m1 = Matrix([[0., 2., 4.], [1., 3., 5.]])
 	assert m1.shape == (2, 3)
 
@@ -22,21 +24,48 @@ def main() -> None:
 	# Let's test matrix multiplication
 
 	m1 = Matrix([[0.0, 1.0, 2.0, 3.0],
-				 [0.0, 2.0, 4.0, 6.0]])
+	             [0.0, 2.0, 4.0, 6.0]])
 	m2 = Matrix([[0.0, 1.0],
-				 [2.0, 3.0],
-				 [4.0, 5.0],
-				 [6.0, 7.0]])
+	             [2.0, 3.0],
+	             [4.0, 5.0],
+	             [6.0, 7.0]])
 
 	matmult = m1 * m2
-	print(matmult)
 	assert matmult.shape == (2, 2)
 	assert matmult.data == [[28., 34.], [56., 68]]
 
+	m1_plus_m1 = m1 + m1
+	assert m1_plus_m1.shape == m1.shape
+	assert m1_plus_m1.data == [[0., 2., 4., 6.], [0., 4., 8., 12.]]
+
+	v1 = Vector([[1, 2, 3]])  # create a row vector
+	assert v1.shape == (1, 3)
+	v2 = Vector([[1], [2], [3]])  # create a column vector
+	assert v2.shape == (3, 1)
+
+	try:
+		v3 = Vector([[1, 2], [3, 4]])  # return an error
+		assert False, 'v3 = Vector([[1, 2], [3, 4]]) should have raised an error!'
+	except TypeError:
+		pass
+
 	m1 = Matrix([[0.0, 1.0, 2.0],
-				 [0.0, 2.0, 4.0]])
+	             [0.0, 2.0, 4.0]])
 	v1 = Vector([[1], [2], [3]])
-	m1 * v1
+	v1_m1 = m1 * v1
+	assert isinstance(v1_m1, Vector)
+	assert v1_m1.shape == (m1.shape[0], v1.shape[1])
+
+	v1 = Vector([[1], [2], [3]])
+	v2 = Vector([[2], [4], [8]])
+	v12 = v1 + v2
+	# Output:
+	assert v12.shape == (3, 1)
+	assert v12.data == [[3], [6], [11]]
+
+	dot = v1.dot(v2)
+	assert isinstance(dot, (int, float))
+	assert dot == 1 * 2 + 2 * 4 + 3 * 8  # 34
 
 
 if __name__ == '__main__':
