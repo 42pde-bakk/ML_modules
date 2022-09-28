9f14d390086824bb5326db22690e764856a60d0d
pde-bakk <pde-bakk@student.codam.nl>
2022-09-28 20:03:49 +0200

    better formatting for tests

--- module00/ex00/test.py
+++ module00/ex00/test.py
@@ -4,7 +4,11 @@ from matrix import Matrix, Vector
 def main() -> None:
 	# Simple constructor and Matrix.T() tests
 	assert Matrix((3, 3)).shape == (3, 3), 'Error in Matrix((3, 3))'
-	m1 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+	m1 = Matrix([
+		[0.0, 1.0],
+		[2.0, 3.0],
+		[4.0, 5.0]
+	])
 	assert m1.shape == (3, 2), 'Shape wrong'
 	assert m1.data == [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]]
 
@@ -14,7 +18,10 @@ def main() -> None:
 
 	# Maybe also do some tests on __repr__ and __str__ ?
 
-	m1 = Matrix([[0., 2., 4.], [1., 3., 5.]])
+	m1 = Matrix([
+		[0., 2., 4.],
+		[1., 3., 5.]
+	])
 	assert m1.shape == (2, 3)
 
 	m1_t = m1.T()
@@ -23,12 +30,16 @@ def main() -> None:
 
 	# Let's test matrix multiplication
 
-	m1 = Matrix([[0.0, 1.0, 2.0, 3.0],
-	             [0.0, 2.0, 4.0, 6.0]])
-	m2 = Matrix([[0.0, 1.0],
-	             [2.0, 3.0],
-	             [4.0, 5.0],
-	             [6.0, 7.0]])
+	m1 = Matrix([
+		[0.0, 1.0, 2.0, 3.0],
+		[0.0, 2.0, 4.0, 6.0]
+	])
+	m2 = Matrix([
+		[0.0, 1.0],
+		[2.0, 3.0],
+		[4.0, 5.0],
+		[6.0, 7.0]
+	])
 
 	matmult = m1 * m2
 	assert matmult.shape == (2, 2)
@@ -38,9 +49,15 @@ def main() -> None:
 	assert m1_plus_m1.shape == m1.shape
 	assert m1_plus_m1.data == [[0., 2., 4., 6.], [0., 4., 8., 12.]]
 
-	v1 = Vector([[1, 2, 3]])  # create a row vector
+	v1 = Vector([
+		[1, 2, 3]
+	])  # create a row vector
 	assert v1.shape == (1, 3)
-	v2 = Vector([[1], [2], [3]])  # create a column vector
+	v2 = Vector([
+		[1],
+		[2],
+		[3]
+	])  # create a column vector
 	assert v2.shape == (3, 1)
 
 	try:
@@ -49,15 +66,29 @@ def main() -> None:
 	except TypeError:
 		pass
 
-	m1 = Matrix([[0.0, 1.0, 2.0],
-	             [0.0, 2.0, 4.0]])
-	v1 = Vector([[1], [2], [3]])
+	m1 = Matrix([
+		[0.0, 1.0, 2.0],
+		[0.0, 2.0, 4.0]
+	])
+	v1 = Vector([
+		[1],
+		[2],
+		[3]
+	])
 	v1_m1 = m1 * v1
 	assert isinstance(v1_m1, Vector)
 	assert v1_m1.shape == (m1.shape[0], v1.shape[1])
 
-	v1 = Vector([[1], [2], [3]])
-	v2 = Vector([[2], [4], [8]])
+	v1 = Vector([
+		[1],
+		[2],
+		[3]
+	])
+	v2 = Vector([
+		[2],
+		[4],
+		[8]
+	])
 	v12 = v1 + v2
 	# Output:
 	assert v12.shape == (3, 1)
