aaa9b0c912ec524f65a90fca9d170436032e5925
pde-bakk <pde-bakk@student.codam.nl>
2022-09-29 16:23:47 +0200

    vectorized gradient

---
+++ module01/ex01/gradient.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def accepts(*types):
+	def check_accepts(f):
+		if len(types) != f.__code__.co_argcount:
+			return None
+
+		def new_f(*args, **kwargs):
+			if any(not isinstance(arg, t) for arg, t in zip(args, types)):
+				return None
+			return f(*args, **kwargs)
+		# new_f.__name__ = f.__name__
+		return new_f
+	return check_accepts
+
+
+@accepts(np.ndarray, np.ndarray, np.ndarray)
+def simple_gradient(x: np.ndarray, y: np.ndarray, theta: np.ndarray) -> np.ndarray | None:
+	"""Computes a gradient vector from three non-empty numpy.array, with a for-loop.
+	The three arrays must have compatible shapes.
+	Args:
+	x: has to be an numpy.array, a vector of shape m * 1.
+	y: has to be an numpy.array, a vector of shape m * 1.
+	theta: has to be an numpy.array, a 2 * 1 vector.
+	Return:
+	The gradient as a numpy.array, a vector of shape 2 * 1.
+	None if x, y, or theta are empty numpy.array.
+	None if x, y and theta do not have compatible shapes.
+	Raises:
+	This function should not raise any Exception.
+	"""
+	if any(a.size == 0 for a in [x, y, theta]):
+		return None
+	m = y.size
+	x = np.hstack((np.ones_like(x), x))
+	return (1 / m) * (x.transpose().dot(x.dot(theta) - y))
---
+++ module01/ex01/test.py
@@ -0,0 +1,27 @@
+import numpy as np
+from gradient import simple_gradient
+
+
+def main() -> None:
+	x = np.array([12.4956442, 21.5007972, 31.5527382, 48.9145838, 57.5088733]).reshape((-1, 1))
+	y = np.array([37.4013816, 36.1473236, 45.7655287, 46.6793434, 59.5585554]).reshape((-1, 1))
+
+	# Example 0:
+	theta1 = np.array([2, 0.7]).reshape((-1, 1))
+	res = simple_gradient(x, y, theta1)
+	a = np.array([
+		[-19.0342574],
+		[-586.66875564]
+	])
+	assert np.allclose(res, a)
+
+	# Example 1:
+	theta2 = np.array([1, -0.4]).reshape((-1, 1))
+	assert np.allclose(simple_gradient(x, y, theta2), np.array([
+		[-57.86823748],
+		[-2230.12297889]
+	]))
+
+
+if __name__ == '__main__':
+	main()
