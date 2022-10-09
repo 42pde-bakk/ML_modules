d7049613aa9b00167a3fc95f4e70d82adc275dc2
pde-bakk <pde-bakk@student.codam.nl>
2022-10-09 21:30:23 +0200

    n minus 1

--- module00/ex01/TinyStatistician.py
+++ module00/ex01/TinyStatistician.py
@@ -62,7 +62,7 @@ class TinyStatistician:
 	@accepts(np.ndarray)
 	def var(x: np.ndarray) -> float | None:
 		mean = TinyStatistician.mean(x)
-		return sum([(elem - mean) ** 2 for elem in x]) / x.shape[0]
+		return sum([(elem - mean) ** 2 for elem in x]) / (x.shape[0] - 1)
 
 	@staticmethod
 	@accepts(np.ndarray)
@@ -70,24 +70,24 @@ class TinyStatistician:
 		return sqrt(TinyStatistician.var(x))
 
 
-if __name__ == '__main__':
-	tstat = TinyStatistician
+def main() -> None:
+	tstat = TinyStatistician()
 	a = np.array([1, 42, 300, 10, 59])
 	mean = tstat.mean(a)
 	print(f'{mean = }')
-	assert mean == 82.4, f'I came up with {mean}'
+	assert mean == 82.4
 	median = tstat.median(a)
 	print(f'{median = }')
-	assert median == 42.0, f'I came up with {median}'
+	assert median == 42.0
 	quartiles = tstat.quartiles(a)
 	print(f'{quartiles = }')
-	assert quartiles == [10.0, 59.0], f'I came up with {quartiles}'
+	assert quartiles == [10.0, 59.0]
 	variance = tstat.var(a)
 	print(f'{variance = }')
-	assert variance == 12279.439999999999, f'I came up with {variance}'
+	assert variance == 15349.3
 	std_deviation = tstat.std(a)
 	print(f'{std_deviation = }')
-	assert std_deviation == 110.81263465868862, f'I came up with {std_deviation}'
+	assert std_deviation == 123.89229193133849
 
 	percentiles: dict = {
 		10: tstat.percentile(a, 10),
@@ -98,3 +98,7 @@ if __name__ == '__main__':
 	assert percentiles[10] == 4.6, f'I came up with {percentiles[10]}'
 	assert percentiles[15] == 6.4, f'I came up with {percentiles[15]}'
 	assert percentiles[20] == 8.2, f'I came up with {percentiles[20]}'
+
+
+if __name__ == '__main__':
+	main()
