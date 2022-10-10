9e25852253ab76308539d2a18ff46e63cbc54eca
pde-bakk <pde-bakk@student.codam.nl>
2022-10-10 09:17:27 +0200

    push cus leaving

---
+++ module01/ex02/fit.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def accepts(*types):
+	def check_accepts(f):
+		if len(types) != f.__code__.co_argcount:
+			return None
+
+		def new_f(*args, **kwargs):
+			if any(not isinstance(arg, t) for arg, t in zip(args, types)):
+				return None
+			return f(*args, **kwargs)
+		# new_f.__name__ = f.__name__
+		return new_f
+	return check_accepts
+
+
+@accepts(np.ndarray, np.ndarray, np.ndarray, int)
+def fit_(x: np.ndarray, y: np.ndarray, theta: np.ndarray, alpha: np.ndarray, max_iter: int) -> np.ndarray:
+	"""
+	Description:
+	Fits the model to the training dataset contained in x and y.
+	Args:
+	x: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+	y: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+	theta: has to be a numpy.ndarray, a vector of dimension 2 * 1.
+	alpha: has to be a float, the learning rate
+	max_iter: has to be an int, the number of iterations done during the gradient descent
+	Returns:
+	new_theta: numpy.ndarray, a vector of dimension 2 * 1.
+	None if there is a matching dimension problem.
+	Raises:
+	This function should not raise any Exception.
+	"""
+	pass
