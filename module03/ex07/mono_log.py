189d59d91c81a1af41585824bb5483c3f286061f
pde-bakk <pde-bakk@student.codam.nl>
2022-10-15 21:33:36 +0200

    mono_log done

---
+++ module03/ex07/mono_log.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import argparse
+from data_splitter import data_splitter
+from my_logistic_regression import MyLogisticRegression as MyLR
+from matplotlib import pyplot as plt
+
+
+INFORMATION_CSV_FILEPATH = '../resources/solar_system_census.csv'
+PLANETS_CSV_FILEPATH = '../resources/solar_system_census_planets.csv'
+
+
+def get_zipcode() -> int:
+	parser = argparse.ArgumentParser(description='Give me a zipcode')
+	parser.add_argument('-zipcode', type=int)
+
+	args = parser.parse_args()
+	if args.zipcode is None or not 0 <= args.zipcode <= 3:
+		parser.print_usage()
+		exit(1)
+	return args.zipcode
+
+
+def prepare_data():
+	data_x = pd.read_csv(INFORMATION_CSV_FILEPATH)
+	data_y = pd.read_csv(PLANETS_CSV_FILEPATH)
+	data_x.drop('Unnamed: 0', axis=1, inplace=True), data_y.drop('Unnamed: 0', axis=1, inplace=True)
+	entire_x = data_x.to_numpy().reshape(-1, len(data_x.columns))
+	entire_y = data_y.to_numpy().reshape(-1, len(data_y.columns))
+	return data_splitter(entire_x, entire_y, 0.8), len(data_x.columns)
+
+
+def main():
+	features = ['weight', 'height', 'bone_density']
+	favourite_zipcode: int = get_zipcode()
+	(x_train, x_test, y_train, y_test), column_len = prepare_data()
+
+	y2_train = (y_train == favourite_zipcode).astype(int)
+	y2_test = (y_test == favourite_zipcode).astype(int)
+
+	initial_thetas = np.ones(shape=(column_len + 1, 1))
+	model = MyLR(initial_thetas, alpha=0.0001, max_iter=10_000)
+	model.fit_(x_train, y2_train)
+	print(f'new thetas: {model.theta}')
+	predictions = model.predict_(x_test)
+	loss = model.loss_(y2_test, predictions)
+	print(f'loss = {loss}')
+	correct = (np.round(predictions) == y2_test).sum()
+	print(f'Correctly predicted values {correct} / {y2_test.shape[0]} = {correct / y2_test.shape[0] * 100}%')
+
+	for idx, feature in enumerate(features):
+		plt.title(feature)
+		column = x_test[:, idx]
+		plt.scatter(column, y2_test, label='Real values')
+		plt.scatter(column, predictions, label='My prediction')
+		plt.scatter(column, np.round(predictions), label='My rounded prediction')
+
+		plt.legend(loc='best')
+		plt.show()
+
+
+
+
+if __name__ == '__main__':
+	main()
---
+++ module03/ex07/my_logistic_regression.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+def accepts(*types):
+	def check_accepts(f):
+		if len(types) != f.__code__.co_argcount:
+			return None
+
+		def new_f(*args, **kwargs):
+			if any(not isinstance(arg, t) for arg, t in zip(args, types)):
+				return None
+			return f(*args, **kwargs)
+
+		# new_f.__name__ = f.__name__
+		return new_f
+
+	return check_accepts
+
+
+class MyLogisticRegression:
+	"""
+	Description: My personal logistic regression to classify things.
+	"""
+
+	def __init__(self, theta: np.ndarray, alpha: float = 0.001, max_iter: int = 1000):
+		if not isinstance(theta, np.ndarray) or not isinstance(alpha, float) or not isinstance(max_iter, int):
+			raise TypeError('Bad arguments given to MyLogisticRegression')
+		self.alpha = alpha
+		self.max_iter = max_iter
+		self.theta = theta
+
+	@staticmethod
+	@accepts(np.ndarray)
+	def sigmoid_(x: np.ndarray) -> np.ndarray:
+		"""
+		Compute the sigmoid of a vector.
+		Args:
+		x: has to be a numpy.ndarray of shape (m, 1).
+		Returns:
+		The sigmoid value as a numpy.ndarray of shape (m, 1).
+		None if x is an empty numpy.ndarray.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		return 1 / (1 + np.exp(-x))
+
+	# @accepts(np.ndarray, np.ndarray, np.ndarray)
+	def gradient_(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+		"""Computes a gradient vector from three non-empty numpy.ndarray, with a for-loop. The three arrays must have compatiblArgs:
+		x: has to be an numpy.ndarray, a matrix of shape m * n.
+		y: has to be an numpy.ndarray, a vector of shape m * 1.
+		theta: has to be an numpy.ndarray, a vector of shape (n + 1) * 1.
+		Returns:
+		The gradient as a numpy.ndarray, a vector of shape n * 1, containing the result of the formula for all j.
+		None if x, y, or theta are empty numpy.ndarray.
+		None if x, y and theta do not have compatible dimensions.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		ones = np.ones(shape=(x.shape[0], 1))
+		x = np.hstack((ones, x))
+		y_hat = MyLogisticRegression.sigmoid_(x.dot(self.theta))
+		return x.T.dot(y_hat - y) / y.shape[0]
+
+	def __gradient_(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+		y_hat = MyLogisticRegression.sigmoid_(x.dot(self.theta))
+		return x.T.dot(y_hat - y) / y.shape[0]
+
+	def fit_(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+		"""
+		:param x: np.ndarray
+		:param y: np.ndarray
+		:return: new theta values
+		"""
+		x = np.column_stack((np.ones(shape=(x.shape[0], 1)), x))
+		for idx in range(self.max_iter):
+			self.theta -= (self.alpha * self.__gradient_(x, y))
+		return self.theta
+
+	@staticmethod
+	@accepts(np.ndarray, np.ndarray, float)
+	def loss_elem_(y: np.ndarray, y_hat: np.ndarray, eps: float = 1e-15) -> np.ndarray | None:
+		"""
+		:param y: Actual values as an np.ndarray
+		:param y_hat: Predicted values as an np.ndarray
+		:param eps: very small value
+		:return: np.ndarray of the losses
+		"""
+		if y.shape != y_hat.shape:
+			return None
+		return y * np.log(y_hat + eps) + (1 - y) * np.log(1 - y_hat + eps)
+
+	@staticmethod
+	@accepts(np.ndarray, np.ndarray, float)
+	def loss_(y: np.ndarray, y_hat: np.ndarray, eps: float = 1e-15) -> float:
+		"""
+		Computes the logistic loss value.
+		Args:
+		y: has to be an numpy.ndarray, a vector of shape m * 1.
+		y_hat: has to be an numpy.ndarray, a vector of shape m * 1.
+		eps: has to be a float, epsilon (default=1e-15)
+		Returns:
+		The logistic loss value as a float.
+		None on any error.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		loss_elem = MyLogisticRegression.loss_elem_(y, y_hat, eps)
+		return -loss_elem.sum() / y.shape[0]
+
+	# @accepts(np.ndarray)
+	def predict_(self, x: np.ndarray) -> np.ndarray:
+		"""Computes the vector of prediction y_hat from two non-empty numpy.ndarray.
+		Args:
+		x: has to be an numpy.ndarray, a vector of dimension m * n.
+		theta: has to be an numpy.ndarray, a vector of dimension (n + 1) * 1.
+		Returns:
+		y_hat as a numpy.ndarray, a vector of dimension m * 1.
+		None if x or theta are empty numpy.ndarray.
+		None if x or theta dimensions are not appropriate.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		ones = np.ones(shape=(x.shape[0], 1))
+		x = np.hstack((ones, x))
+		return MyLogisticRegression.sigmoid_(x.dot(self.theta))
