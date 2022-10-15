0c10c950e468186adeda6f8351ebd30d5a4e4741
pde-bakk <pde-bakk@student.codam.nl>
2022-10-15 19:21:22 +0200

    im dumb

--- module03/ex04/log_gradient.py
+++ module03/ex04/log_gradient.py
@@ -10,8 +10,10 @@ def accepts(*types):
 			if any(not isinstance(arg, t) for arg, t in zip(args, types)):
 				return None
 			return f(*args, **kwargs)
+
 		# new_f.__name__ = f.__name__
 		return new_f
+
 	return check_accepts
 
 
@@ -30,19 +32,20 @@ def sigmoid_(x: np.ndarray) -> np.ndarray:
 	return 1 / (1 + np.exp(-x))
 
 
-@accepts(np.ndarray, np.ndarray)
-def logistic_predict_(x: np.ndarray, theta: np.ndarray) -> np.ndarray:
-	"""Computes the vector of prediction y_hat from two non-empty numpy.ndarray.
-	Args:
-	x: has to be an numpy.ndarray, a vector of dimension m * n.
-	theta: has to be an numpy.ndarray, a vector of dimension (n + 1) * 1.
+@accepts(np.ndarray, np.ndarray, np.ndarray)
+def log_gradient(x: np.ndarray, y: np.ndarray, theta: np.ndarray) -> np.ndarray:
+	"""Computes a gradient vector from three non-empty numpy.ndarray, with a for-loop. The three arrays must have compatiblArgs:
+	x: has to be an numpy.ndarray, a matrix of shape m * n.
+	y: has to be an numpy.ndarray, a vector of shape m * 1.
+	theta: has to be an numpy.ndarray, a vector of shape (n + 1) * 1.
 	Returns:
-	y_hat as a numpy.ndarray, a vector of dimension m * 1.
-	None if x or theta are empty numpy.ndarray.
-	None if x or theta dimensions are not appropriate.
+	The gradient as a numpy.ndarray, a vector of shape n * 1, containing the result of the formula for all j.
+	None if x, y, or theta are empty numpy.ndarray.
+	None if x, y and theta do not have compatible dimensions.
 	Raises:
 	This function should not raise any Exception.
 	"""
 	ones = np.ones(shape=(x.shape[0], 1))
 	x = np.hstack((ones, x))
-	return sigmoid_(x.dot(theta))
+	y_hat = sigmoid_(x.dot(theta))
+	return x * (y_hat - y) / y.shape[0]
