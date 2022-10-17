ab75af0d05ca1a3ad6f46f8000c10e219e643e5f
pde-bakk <pde-bakk@student.codam.nl>
2022-10-17 17:43:25 +0200

    prettified plots

--- module03/ex07/mono_log.py
+++ module03/ex07/mono_log.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import argparse
+import sys
 from data_splitter import data_splitter
 from my_logistic_regression import MyLogisticRegression as MyLR
 from matplotlib import pyplot as plt
@@ -22,8 +23,12 @@ def get_zipcode() -> int:
 
 
 def prepare_data():
-	data_x = pd.read_csv(INFORMATION_CSV_FILEPATH)
-	data_y = pd.read_csv(PLANETS_CSV_FILEPATH)
+	try:
+		data_x = pd.read_csv(INFORMATION_CSV_FILEPATH)
+		data_y = pd.read_csv(PLANETS_CSV_FILEPATH)
+	except (FileNotFoundError, pd.errors.EmptyDataError) as e:
+		print(f'Error: {e}')
+		sys.exit(1)
 	data_x.drop('Unnamed: 0', axis=1, inplace=True), data_y.drop('Unnamed: 0', axis=1, inplace=True)
 	entire_x = data_x.to_numpy().reshape(-1, len(data_x.columns))
 	entire_y = data_y.to_numpy().reshape(-1, len(data_y.columns))
@@ -31,22 +36,28 @@ def prepare_data():
 
 
 def main():
+	zipcode_amount = 4
+	zipcode_names = ['Venus (0)', 'Earth (1)', 'Mars (2)', 'The Belt (3)']
 	features = ['weight', 'height', 'bone_density']
 	favourite_zipcode: int = get_zipcode()
+	ytick_labels = ['Not ' + zipcode_names[favourite_zipcode], zipcode_names[favourite_zipcode]]
 	(x_train, x_test, y_train, y_test), column_len = prepare_data()
 
 	y2_train = (y_train == favourite_zipcode).astype(int)
 	y2_test = (y_test == favourite_zipcode).astype(int)
 
 	initial_thetas = np.ones(shape=(column_len + 1, 1))
-	model = MyLR(initial_thetas, alpha=0.0001, max_iter=300_000)
+	model = MyLR(initial_thetas, alpha=0.0001, max_iter=500_000)
 	model.fit_(x_train, y2_train)
 	predictions = model.predict_(x_test)
-	correct = (np.round(predictions) == y2_test).sum()
+	correct = np.sum(np.round(predictions) == y2_test)
 	print(f'Correctly predicted values {correct} / {y2_test.shape[0]} = {correct / y2_test.shape[0] * 100}%')
 
 	for idx, feature in enumerate(features):
-		plt.title(feature)
+		plt.title(f'Zipcode predicted based on {feature}')
+		plt.xlabel(feature.capitalize())
+		plt.ylabel('Zipcode')
+		plt.yticks(range(0, 2), ytick_labels)
 		column = x_test[:, idx]
 		plt.scatter(column, y2_test, label='Real values', s=200)
 		plt.scatter(column, predictions, label='My prediction', s=50)
--- module03/ex07/multi_log.py
+++ module03/ex07/multi_log.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import sys
 from data_splitter import data_splitter
 from my_logistic_regression import MyLogisticRegression as MyLR
 from matplotlib import pyplot as plt
@@ -10,31 +11,37 @@ PLANETS_CSV_FILEPATH = '../resources/solar_system_census_planets.csv'
 
 
 def prepare_data():
-	data_x = pd.read_csv(INFORMATION_CSV_FILEPATH)
-	data_y = pd.read_csv(PLANETS_CSV_FILEPATH)
+	try:
+		data_x = pd.read_csv(INFORMATION_CSV_FILEPATH)
+		data_y = pd.read_csv(PLANETS_CSV_FILEPATH)
+	except (FileNotFoundError, pd.errors.EmptyDataError) as e:
+		print(f'Error: {e}')
+		sys.exit(1)
 	data_x.drop('Unnamed: 0', axis=1, inplace=True), data_y.drop('Unnamed: 0', axis=1, inplace=True)
 	entire_x = data_x.to_numpy().reshape(-1, len(data_x.columns))
 	entire_y = data_y.to_numpy().reshape(-1, len(data_y.columns))
 	return data_splitter(entire_x, entire_y, 0.8), len(data_x.columns)
 
 
-def create_mono_log_model(zipcode: int, x_train, y_train, x_test, y_test, column_len) -> MyLR:
+def create_mono_log_model(zipcode: int, x_train, y_train, y_test, column_len) -> MyLR:
 	y2_train = (y_train == zipcode).astype(int)
 	y2_test = (y_test == zipcode).astype(int)
 
 	initial_thetas = np.ones(shape=(column_len + 1, 1))
-	model = MyLR(initial_thetas, alpha=0.0001, max_iter=300_000)
+	model = MyLR(initial_thetas, alpha=0.0001, max_iter=500_000)
 	model.fit_(x_train, y2_train)
 	return model
 
 
 def main():
+	zipcode_amount = 4
+	zipcode_names = ['Venus (0)', 'Earth (1)', 'Mars (2)', 'The Belt (3)']
 	features = ['weight', 'height', 'bone_density']
 	(x_train, x_test, y_train, y_test), column_len = prepare_data()
 
 	models, preds = [], []
-	for zipcode in range(4):
-		model = create_mono_log_model(zipcode, x_train, y_train, x_test, y_test, column_len)
+	for zipcode in range(zipcode_amount):
+		model = create_mono_log_model(zipcode, x_train, y_train, y_test, column_len)
 		models.append(model)
 		preds.append(model.predict_(x_test))
 
@@ -50,7 +57,10 @@ def main():
 	print(f'Correctly predicted values {correct_predictions} / {y_test.shape[0]} = {correct_predictions / y_test.shape[0] * 100}%')
 
 	for idx, feature in enumerate(features):
-		plt.title(feature)
+		plt.title(f'Zipcode plotted against {feature.capitalize()}')
+		plt.xlabel(feature.capitalize())
+		plt.ylabel('Zipcode')
+		plt.yticks(range(0, zipcode_amount), zipcode_names)
 		column = x_test[:, idx]
 		plt.scatter(column, y_test, label='Real values', s=200)
 		plt.scatter(column, final_preds, label='My prediction', s=50)
