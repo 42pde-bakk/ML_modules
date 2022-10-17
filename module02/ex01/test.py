10bc61229005409e72154a3b7a1f51efe6ec594a
pde-bakk <pde-bakk@student.codam.nl>
2022-10-17 11:43:02 +0200

    reformatted answer matrices in ex00 and ex01

--- module02/ex01/test.py
+++ module02/ex01/test.py
@@ -9,7 +9,12 @@ def main() -> None:
 	print('Example 1:')
 	theta1 = np.array([5, 0, 0, 0]).reshape((-1, 1))
 	y_hat = simple_predict(X, theta1)
-	answer = np.array([[5.], [5.], [5.], [5.]])
+	answer = np.array([
+		[5.],
+		[5.],
+		[5.],
+		[5.]
+	])
 	print(f'{y_hat = }')
 	assert np.allclose(y_hat, answer)
 	# Do you understand why y_hat contains only 5’s here?
@@ -18,7 +23,12 @@ def main() -> None:
 	print('\n\nExample 2:')
 	theta2 = np.array([0, 1, 0, 0]).reshape((-1, 1))
 	y_hat = simple_predict(X, theta2)
-	answer = np.array([[1.], [4.], [7.], [10.]])
+	answer = np.array([
+		[1.],
+		[4.],
+		[7.],
+		[10.]
+	])
 	print(f'{y_hat = }')
 	assert np.allclose(y_hat, answer)
 	# Do you understand why y_hat == x[:,0] here?
@@ -27,14 +37,24 @@ def main() -> None:
 	print('\n\nExample 3:')
 	theta3 = np.array([-1.5, 0.6, 2.3, 1.98]).reshape((-1, 1))
 	y_hat = simple_predict(X, theta3)
-	answer = np.array([[9.64], [24.28], [38.92], [53.56]])
+	answer = np.array([
+		[9.64],
+		[24.28],
+		[38.92],
+		[53.56]
+	])
 	print(f'{y_hat = }')
 	assert np.allclose(y_hat, answer)
 
 	# Example 4:
 	theta4 = np.array([-3, 1, 2, 3.5]).reshape((-1, 1))
 	y_hat = simple_predict(X, theta4)
-	answer = np.array([[12.5], [32.], [51.5], [71.]])
+	answer = np.array([
+		[12.5],
+		[32.],
+		[51.5],
+		[71.]
+	])
 	print(f'{y_hat = }')
 	assert np.allclose(y_hat, answer)
 
