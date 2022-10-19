4ef4f7b90207751df8a93e4cd1a9128121845408
pde-bakk <pde-bakk@student.codam.nl>
2022-10-19 11:59:48 +0200

    made actual cross validation, not the bullshit that the subject asks for

--- module04/ex07/benchmark_train.py
+++ module04/ex07/benchmark_train.py
@@ -5,21 +5,20 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from ridge import MyRidge
-from data_splitter import data_splitter
-from typing import Tuple
+from cross_validation import build_cross_validation_sets
 
 
 THETAS_FILE = 'models.pickle'
 CSV_FILE_PATH = '../resources/space_avocado.csv'
 
 
-def prepare_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def prepare_data() -> list[dict]:
 	features = ['weight', 'prod_distance', 'time_delivery']
 	data = pd.read_csv(CSV_FILE_PATH)
 	data.drop('Unnamed: 0', axis=1, inplace=True)
 	x = data[features].to_numpy().reshape(-1, 3)
 	y = data['target'].to_numpy().reshape(-1, 1)
-	return data_splitter(x, y, proportion=0.7, crossval_proprtion=0.15)
+	return build_cross_validation_sets(x, y, 0.75)
 
 
 def plot_mse_scores(losses: list, title: str):
@@ -34,24 +33,36 @@ def plot_mse_scores(losses: list, title: str):
 	plt.show()
 
 
-def benchmark_train(x: np.ndarray, y: np.ndarray):
+def benchmark_train(cross_validation_sets: list[dict]):
 	models, losses = [], []
 	for i in range(1, 5):
 		print(f'i = {i}\n')
-		new_x = MyRidge.add_polynomial_features(x, i)
-		new_x = MyRidge.zscore(new_x)
-		thetas = np.ones(shape=(new_x.shape[1] + 1, 1))
-		print(f'created new_x with shape={new_x.shape}, {thetas.shape=}')
+		thetas = np.ones(shape=(3 * i + 1, 1))
+		print(f'created {thetas.shape=}')
 		lambda_range = np.arange(0.0, 1.2, step=0.2)
 		for lambda_ in lambda_range:
 			print(f'Let\'s train a new model, polynomial={i}, λ {lambda_=}')
-			model = MyRidge(thetas, alpha=0.001, max_iter=1_000_000, lambda_=lambda_)
-			model.fit_(new_x, y)
-			loss = model.loss_(y, model.predict_(new_x))
-			models.append(model)
-			losses.append(loss)
-			print(f'Model {i} training loss: {loss}')
-		# break
+			model = MyRidge(thetas, alpha=0.001, max_iter=10_000, lambda_=lambda_)
+			cross_validation_losses = []
+			for idx, sets in enumerate(cross_validation_sets):
+				model.set_params(thetas=thetas)  # Reset thetas
+				x_train, y_train = sets['x']['training'], sets['y']['training']
+				x_train = MyRidge.add_polynomial_features(x_train, i)
+				x_train = MyRidge.zscore(x_train)
+
+				print(f'Training Model {i} with λ {lambda_=}', end='')
+				model.fit_(x_train, y_train)
+
+				x_test, y_test = sets['x']['testing'], sets['y']['testing']
+				x_test = MyRidge.zscore(MyRidge.add_polynomial_features(x_test, i))
+
+				y_hat = model.predict_(x_test)
+				loss = model.loss_(y_test, y_hat)
+				print(f' has a loss of {loss}.')
+				cross_validation_losses.append(loss)
+			average_loss = sum(cross_validation_losses) / len(cross_validation_losses)
+			print(f'Model {i} with λ {lambda_=} had an average loss of {average_loss}')
+			losses.append(average_loss)
 
 	thetas = [lr.thetas for lr in models]
 	with open(THETAS_FILE, 'wb') as handle:
@@ -60,7 +71,4 @@ def benchmark_train(x: np.ndarray, y: np.ndarray):
 
 
 if __name__ == '__main__':
-	x_train, x_crossval, x_test, y_train, y_crossval, y_test = prepare_data()
-	print(x_train.shape, x_crossval.shape, x_test.shape)
-	print(y_train.shape, y_crossval.shape, y_test.shape)
-	benchmark_train(x_train, y_train)
+	benchmark_train(prepare_data())
---
+++ module04/ex07/cross_validation.py
@@ -0,0 +1,55 @@
+import sys
+import numpy as np
+
+
+def accepts(*types):
+	def check_accepts(f):
+		if len(types) != f.__code__.co_argcount:
+			return None
+
+		def new_f(*args, **kwargs):
+			if any(not isinstance(arg, t) for arg, t in zip(args, types)):
+				return None
+			return f(*args, **kwargs)
+
+		# new_f.__name__ = f.__name__
+		return new_f
+
+	return check_accepts
+
+
+@accepts(np.ndarray, np.ndarray, float)
+def build_cross_validation_sets(x: np.ndarray, y: np.ndarray, proportion: float) -> list | None:
+	"""
+	:param x: np.ndarray of shape m * n
+	:param y: np.ndarray of shape m * 1
+	:param proportion: non-null postive float lower than 1.0
+	:return: list of dictionaries containing the training and testing datasets for x and y
+	"""
+	if x.shape[0] != y.shape[0]:
+		print(f'Why are you giving me arrays of differing sizes?', file=sys.stderr)
+		return
+	test_proportion: float = 1.0 - proportion
+	cross_validation_sets = []
+	for start in np.arange(0.0, 1.0, step=test_proportion):
+		end = start + test_proportion
+		if end > 1.0:
+			print(f'end is bigger than 1.0')
+			break
+		x_train_set_0, x_test_set, x_train_set_1 = np.split(x, [int(start * x.shape[0]), int(end * x.shape[0])])
+		y_train_set_0, y_test_set, y_train_set_1 = np.split(y, [int(start * x.shape[0]), int(end * x.shape[0])])
+
+		entry = {
+			'x': {
+				'training': np.vstack((x_train_set_0, x_train_set_1)),
+				'testing': x_test_set
+			},
+			'y': {
+				'training': np.vstack((y_train_set_0, y_train_set_1)),
+				'testing': y_test_set
+			}
+		}
+		cross_validation_sets.append(entry)
+
+	return cross_validation_sets
+
